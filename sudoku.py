f3f437bf510d4296741a7212f3c911e466a4f8a3
selmabou <bouabidisalma07@gmail.com>
2024-01-03 12:42:06 +0100

    validation

--- sudoku.py
+++ sudoku.py
@@ -1,39 +1,16 @@
-# for i in range(0,9):
-#     print(" ",i, end="")
-# for i in range (0,9):
-#     print("") 
-#     print(i, end="")
-#     for j in range (0,9):
-#         print(" - ", end="")
 
-
-
-
-# def sudoku(row, col, number):
-#     for i in range(0,9):
-#         print(" ",i, end="")
-#     for i in range (0,9):
-#         for j in range(0,9):
-#             if i == row and j == col:
-#                 print(f"{number}", end="")
-#             else:
-#                 print(" ", end="")
-
-#         print("") 
-#         print(i, end="")
+ListPrincipale =[ 
+        ["4","3","5","2","6","9","7","8","1"],
+        ["6","8","2","5","7","1","4","9","3"],
+        ["1","9","7","8","3","4","5","6","2"],
+        ["8","2","6","1","9","5","3","4","7"],
+        ["3","7","4","6","8","2","9","1","5"],
+        ["9","5","1","7","4","3","6","2","8"],
+        ["5","1","9","3","2","6","8","7","4"],
+        ["2","4","8","9","5","7","1","3","6"],
+        ["7","6","3","4","1","8","2","5","9"],
+       ] 
        
-
-# sudoku(2,5,8)
-
-
-ListPrincipale =[]
-for i in range (9) :
-    ListSecondaire = []
-    for i in range(9):
-        ListSecondaire.append("-")
-    ListPrincipale.append(ListSecondaire)    
-        
-        
 for i in range (9):
     for j in range(9):
         print(ListPrincipale[i][j] , end="   ")
@@ -41,67 +18,56 @@ for i in range (9):
 
 row = int(input("donnez num de row :"))
 col = int(input("donnez num de col :"))
-number = int(input("donnez un nombre :"))
+number = int(input("---> donnez un nombre (  555 --> sorte!!):"))
 
-ListPrincipale[row-1][col-1] = number
+    
+while number != 555 :
+    ListPrincipale[row-1][col-1] = number
+    
+    row = int(input("donnez num de row :"))
+    col = int(input("donnez num de col :"))
+    number = int(input("---> donnez un nombre (  555 --> sorte!!):"))   
+    
+              
+    
 for i in range (9):
     for j in range(9):
         print(ListPrincipale[i][j] , end="   ")
     print()
+    
+    
+    
+#validation :
+rowList = []
+conteur1 = 0
+for i in range(9):
+    rowList = ListPrincipale[i]
+        
+    for j in range(9):
+        for k in range(j+1 , 9):
+            if rowList[k] == rowList[j]:
+                conteur1 += 1
+                
+                           
+if conteur1 != 0 :
+    print("invalide row !!") 
+else : 
+    print("valider row ")                   
+
+conteur2 = 0
+for j in range(9):
+    colList = []
+
+    for i in range(9):
+        colList.append(ListPrincipale[i][j]) 
+
+    for y in range(9):
+        for x in range(y+1 , 9):
+            if colList[y] == colList[x]:
+                conteur2 += 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if conteur2 != 0 :
+    print("invalide col !!")
+else :
+    print("valider col ")  
